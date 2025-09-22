b649cfb5d87440e4aa724f0338e068fc93eb78ea
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-09-22 12:21:18 -0700

    Add conventions.py 80 character limit

---
+++ conventions.py
@@ -0,0 +1,33 @@
+# Help with 80 character limit
+# To set line in VSCode: Hit gear icon > Settings > search for 'ruler'
+#   Select Editor: Rulers: 'Edit in settings.json' > Insert 80 in editor.rulers
+#   list.  Save, and it should show up on-screen.
+
+# Tip #1: Use parentheses to allow line breaks
+# This will crash Python:
+if my_really_long_variable_name == True and my_other_really_long_variable_name 
+        == False:
+# This will not:
+if (my_really_long_variable_name == True and my_other_really_long_variable_name 
+        == False):
+# Same thing works in function definitions and formulas etc.
+
+# If you have a really long print statement
+# Just get the thought out:
+print("I have a lot to say, and I'm really long-winded, and I'm gonna write a run-on sentence because no English teacher will ever read this.")
+# Replace the print( with a variable:
+my_str = "I have a lot to say, and I'm really long-winded, and I'm gonna write a run-on sentence because no English teacher will ever read this.")
+# Make sections of this string as needed:
+my_str = "I have a lot to say, and I'm really long-winded, and I'm gonna "
+my_str += "write a run-on sentence because no English teacher will ever "
+my_str += "read this."
+# Add \n characters so the Terminal gets new lines too:
+# You don't need an extra \n at the end
+my_str = "I have a lot to say, and I'm really long-winded, and I'm gonna "
+my_str += "write a \nrun-on sentence because no English teacher will ever "
+my_str += "read this."
+# Then print() that string
+my_str = "I have a lot to say, and I'm really long-winded, and I'm gonna "
+my_str += "write a \nrun-on sentence because no English teacher will ever "
+my_str += "read this."
+print(my_str) # print() ends with a newline character by default
